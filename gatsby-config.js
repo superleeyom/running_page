76c02ddf18db4ca4afb015a4a16298c365d159bb
superleeyom <635709492@qq.com>
2021-06-03 17:12:28 +0800

    merge upstream update,thanks@yihong0618

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   pathPrefix: '/', // Change to `/running_page` when running on github pages
   siteMetadata: {
-    title: 'Running page',
+    siteTitle: 'Running page',
     siteUrl: 'https://running.leeyom.top',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',

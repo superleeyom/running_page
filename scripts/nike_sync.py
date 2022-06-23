434527cff4ee07926ea4afd30b19cf4abd9fbb54
superleeyom <635709492@qq.com>
2022-06-23 15:26:49 +0800

    :beers: add log

--- scripts/nike_sync.py
+++ scripts/nike_sync.py
@@ -117,6 +117,7 @@ def save_activity(activity):
     path = os.path.join(OUTPUT_DIR, f"{activity_time}.json")
     try:
         with open(path, "w") as f:
+            print(f"debug activity path: {path}")
             json.dump(sanitise_json(activity), f, indent=4)
     except Exception:
         os.unlink(path)

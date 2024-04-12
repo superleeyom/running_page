29eda115729d03b3d4b1491d6b093055b4de8633
superleeyom <635709492@qq.com>
2024-04-12 10:36:52 +0800

    update site info

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,18 +10,18 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  siteTitle: '我的跑步记录日志',
+  siteUrl: 'https://running.leeyom.top',
+  logo: 'https://img2.imgtp.com/2024/04/12/O8DinhDV.jpeg',
+  description: '喜欢强风吹拂的感觉~',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://blog.leeyom.top',
     },
     {
       name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      url: 'https://github.com/superleeyom',
     },
   ],
 };

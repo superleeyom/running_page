48aacaab0b2b5166b6879aa0a029f9e49a482d3b
superleeyom <635709492@qq.com>
2025-01-08 10:15:52 +0800

    add header img

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -12,7 +12,7 @@ interface ISiteMetadataResult {
 const data: ISiteMetadataResult = {
   siteTitle: '我的跑步日志',
   siteUrl: 'https://running.leeyom.top',
-  logo: 'https://img2.imgtp.com/2024/04/12/O8DinhDV.jpeg',
+  logo: 'https://raw.githubusercontent.com/superleeyom/running_page/master/public/images/myheader3.jpeg',
   description: '喜欢强风吹拂的感觉~',
   navLinks: [
     {
